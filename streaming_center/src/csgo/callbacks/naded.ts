c39f741d07f05917a6f99d28e125ed9c13334d38
LuiScreaMed <lui5@qq.com>
2023-04-06 00:52:30 +0800

    - 闪光弹回调添加叉叉眼变化

--- streaming_center/src/csgo/callbacks/naded.ts
+++ streaming_center/src/csgo/callbacks/naded.ts
@@ -1,11 +1,13 @@
 /*
  * @Author: LuiScreaMed lui5@qq.com
- * @LastEditTime: 2023-04-04 23:00:35
+ * @LastEditTime: 2023-04-06 00:51:25
  * Copyright (c) 2023 by LuiScreaMed
  * MIT Licensed
  * @Description: csgo naded event callbacks
  * 被雷的事件回调
  */
+
+import { timeout } from "utils";
 
 ///type直接拿obs中的源名称了
 function getRequestOption(type: "smoke" | "l2d_flashed" | "fire", enabled: boolean) {
@@ -30,6 +32,11 @@ export async function onPlayerSmokedOff() {
 export function onPlayerFlashedOn() {
     // console.log('被闪了');
     let option = getRequestOption("l2d_flashed", true);
+    this.obs?.triggerHotkeyWhenExpressionState({
+        triggerName: "crying",
+        expressionName: "叉叉眼",
+        flag: false
+    });
     this.obs?.setItemEnabled(option);
 }
 
@@ -37,6 +44,12 @@ export function onPlayerFlashedOn() {
 export function onPlayerFlashedOff() {
     // console.log('闪光结束了');
     let option = getRequestOption("l2d_flashed", false);
+    timeout(() =>
+        this.obs?.triggerHotkeyWhenExpressionState({
+            triggerName: "crying",
+            expressionName: "叉叉眼",
+            flag: true
+        }), 500);
     this.obs?.setItemEnabled(option);
 }
 

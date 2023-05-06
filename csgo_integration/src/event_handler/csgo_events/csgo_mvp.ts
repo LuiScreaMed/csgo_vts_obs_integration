2f661cd1c7fc12ccd44e7f77e5c2f50dd7cc96a0
LuiScreaMed <lui5@qq.com>
2023-05-06 22:29:35 +0800

    - 修复mvp次数不重置的问题

--- csgo_integration/src/event_handler/csgo_events/csgo_mvp.ts
+++ csgo_integration/src/event_handler/csgo_events/csgo_mvp.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: LuiScreaMed lui5@qq.com
- * @LastEditTime: 2023-03-31 00:36:27
+ * @LastEditTime: 2023-05-06 22:29:02
  * @Description: MVP事件处理
  * Copyright (c) 2023 by LuiScreaMed
  * MIT Licensed
  */
 
-import { Data, Phase } from "../../data.js";
+import { Data, Map, Phase } from "../../data.js";
 import { Player } from "./event_list.js";
 
 let mvpCount: number = 0;
@@ -22,6 +22,10 @@ export default function mvpEvent(data: Data) {
     roundPhase = phase;
     ///如果已经（上局）是mvp，则取消mvp状态
     if (isMvp) { setMvp.call(this, false); return; }
+    ///如果当前mvp次数为0，则重置本地mvp次数
+    if (mvps === 0) {
+        mvpCount = 0;
+    }
     ///如果mvp次数变多了
     if (mvps <= mvpCount) return;
     mvpCount = mvps;
